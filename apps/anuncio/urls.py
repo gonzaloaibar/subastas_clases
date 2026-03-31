74c64d21eda2a62c32d5e180e8351e2ceebc37d8
arielgonzalo <aibargonzalo0@gmail.com>
2026-03-31 10:26:24 -0300

    trabajo terminado

--- apps/anuncio/urls.py
+++ apps/anuncio/urls.py
@@ -7,6 +7,6 @@ app_name = 'anuncio'
 urlpatterns = [
     path('api-view/categoria/', CategoriaListaAPIView.as_view()),
     path('api-view/categoria/<pk>/', CategoriaDetalleAPIView.as_view()),
-    path('api-view/anuncio/', AnuncioListaAPIView.as_view()),
-    path('api-view/anuncio/<pk>/', AnuncioDetalleAPIView.as_view()),
+    path('api-view/anuncios/', AnuncioListaAPIView.as_view()),
+    path('api-view/anuncios/<pk>/', AnuncioDetalleAPIView.as_view()),
 ]
